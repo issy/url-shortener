2f8c3385b4a810af18a03f03dcffb297071f658e
Issy <isabella.szemeti@gmail.com>
2021-01-25 12:27:38 +0000

    feaat(JS): Added copy functionality

--- static/app.js
+++ static/app.js
@@ -1,7 +1,6 @@
 async function formSubmit() {
     const searchBar = document.getElementById("input-bar")
     let url = searchBar.value;
-    console.log(url);
     payload = { "target_url": url }
     // Make web request
     response = await fetch('/add-link', { method: 'post', body: JSON.stringify(payload) })
@@ -13,58 +12,23 @@ async function formSubmit() {
             element.classList.replace("invisible", name);
         }
     });
+    // Insert the new URL
     outputBox = document.getElementById("output-box");
     outputBox.innerHTML = data.url;
 }
 
-function copyToClipboard() {
-    var copyText = document.getElementById("output-box");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-}
-
-function fallbackCopyTextToClipboard(text) {
-    var textArea = document.createElement("textarea");
-    textArea.value = text;
-
-    // Avoid scrolling to bottom
-    textArea.style.top = "0";
-    textArea.style.left = "0";
-    textArea.style.position = "fixed";
-
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-
-    try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-        console.log('Fallback: Copying text command was ' + msg);
-    } catch (err) {
-        console.error('Fallback: Oops, unable to copy', err);
-    }
-
-    document.body.removeChild(textArea);
-}
-function copyTextToClipboard(text) {
+async function copyTextToClipboard() {
     if (!navigator.clipboard) {
-        fallbackCopyTextToClipboard(text);
         return;
     }
-    navigator.clipboard.writeText(text).then(function () {
-        console.log('Async: Copying to clipboard was successful!');
-    }, function (err) {
-        console.error('Async: Could not copy text: ', err);
-    });
+    textBox = document.getElementById("output-box");
+    navigator.clipboard.writeText(textBox.innerHTML);
+    button = document.getElementById("copy-button");
+    oldText = button.innerHTML;
+    button.classList.add("active");
+    button.innerHTML = 'Copied!';
+    // Sleep for 1 second
+    await new Promise(r => setTimeout(r, 1000));
+    button.classList.remove("active");
+    button.innerHTML = oldText;
 }
-
-copyBobBtn.addEventListener('click', function (event) {
-    textBox =
-        copyTextToClipboard('Bob');
-});
-
-
-copyJaneBtn.addEventListener('click', function (event) {
-    copyTextToClipboard('Jane');
-});
